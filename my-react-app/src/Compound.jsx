5450513bdfe0f909acb284730c639fb0762df8a6
ElochiFighter <elochi.walker@outlook.com>
2026-08-30 17:29:13 -0400

    colors and dotted pattern

--- my-react-app/src/Compound.jsx
+++ my-react-app/src/Compound.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ElementEnum } from './Constants.jsx'
+import { ElementEnum } from './Elements.jsx'
 import './styles/Compound.css'
 
 function Compound() {
@@ -10,8 +10,40 @@ function Compound() {
       <aside id="element-sidebar">
         {elementKeys.map((key) => {
           const element = ElementEnum[key];
+          let color;
+          switch (element.category) {
+            case 'reactive-nonmetal':
+              color = '#ff6b6b';
+              break;
+            case 'noble-gas':
+              color = '#1dd1a1';
+              break;
+            case 'alkali-metal':
+              color = '#feca57';
+              break;
+            case 'alkaline-earth-metal':
+              color = '#48dbfb';
+              break;
+            case 'metalloid':
+              color = '#ff9ff3';
+              break;
+            case 'post-transition-metal':
+              color = '#f368e0';
+              break;
+            case 'transition-metal':
+              color = '#576574';
+              break;
+            case 'lanthanide':
+              color = '#00d2d3';
+              break;
+            case 'actinide':
+              color = '#ff9f43';
+              break;
+            default:
+              color = '#b7b7b7';
+          }
           return (
-            <div key={key} className="element-item">
+            <div key={key} className="element-item" style={{ backgroundColor: color }}>
               <h2>{element.symbol}</h2>
               <p>{element.name}</p>
               <p>{element.atomicNumber}</p>
